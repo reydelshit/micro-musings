16cbe91b125ce43010eeef677e4783ed64c5d8a4
Reydel Ocon <reydel321@gmail.com>
2023-07-27 01:02:41 +0800

    change github provider to google

--- lib/auth-options.ts
+++ lib/auth-options.ts
@@ -1,36 +1,46 @@
+import { NextAuthOptions } from 'next-auth';
+import { prisma } from '../prisma/db';
 
-import { NextAuthOptions } from "next-auth";
-import { prisma } from "../prisma/db";
-
-import GithubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@auth/prisma-adapter';
 
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(prisma) as any,
+  adapter: PrismaAdapter(prisma) as any,
 
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!,
-            
-            profile(profile) {
-                return {
-                    id: String(profile.id),
-                    name: profile.login,
-                    image: profile.avatar_url,
-                }
+  providers: [
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID!,
+    //   clientSecret: process.env.GITHUB_SECRET!,
 
-            }
-        }),
-    ],
-    callbacks: {
-        session: async ({session, user}) => {
-            if(session?.user){
-                session.user.id = user.id
-            }
-            return session
-        }
-    }
+    //   profile(profile) {
+    //     return {
+    //       id: String(profile.id),
+    //       name: profile.login,
+    //       image: profile.avatar_url,
+    //     };
+    //   },
+    // }),
 
-}
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+
+      profile(profile) {
+        return {
+          id: String(profile.id),
+          name: profile.login,
+          image: profile.avatar_url,
+        };
+      },
+    }),
+  ],
+  callbacks: {
+    session: async ({ session, user }) => {
+      if (session?.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+};
